5e58e2a4e5bce98f9ce489825146f285a09398bb
Hugo Correia <hugocorreia1997@gmail.com>
2025-12-16 18:57:33 -0300

    Update module 4

---
+++ Modulo4/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Classes e Métodos
+1.*/
+
+using System;
+
+Carro carro = new Carro();
+carro.Tipo ("Sedan", "Chevrolet", "Onix", 2016, 110);
+carro.Tipo ("SUV", "Ford", "EcoSport", 2018, 120);
+
+Console.ReadLine();
+
+class Carro
+{
+    public string Modelo;
+    public string Montadora;
+    public string Marca;
+    public int Ano;
+    public int Potencia;
+   
+    public void Tipo (string modelo, string montadora, string marca, int ano, int potencia)
+    {
+        Modelo = modelo;
+        Montadora = montadora;
+        Marca = marca;
+        Ano = ano;
+        Potencia = potencia;
+        Console.WriteLine($"Modelo: {Modelo}, Montadora: {Montadora}, Marca: {Marca}, Ano: {Ano}, Potência: {Potencia}cv");
+        Console.WriteLine($"Acelerando o meu {Marca}");
+        Console.WriteLine();
+    }
+
+    
+}
